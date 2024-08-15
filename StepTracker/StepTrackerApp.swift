89a8c5a1ebe070ce99f8c497af449c3fc25d610f
Alexey Kremnev <alkremn@gmail.com>
2024-08-14 18:46:44 -0600

    Added start files

---
+++ StepTracker/StepTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  StepTrackerApp.swift
+//  StepTracker
+//
+//  Created by Alexey Kremnev on 8/14/24.
+//
+
+import SwiftUI
+
+@main
+struct StepTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
